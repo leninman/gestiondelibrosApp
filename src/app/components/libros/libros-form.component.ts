d2abf3595a05436df9c87a3d67e0d23f48e026a0
Lenin <leninmanrique@gmail.com>
2022-06-30 13:47:45 -0400

    Segundo commit 30-06

--- src/app/components/libros/libros-form.component.ts
+++ src/app/components/libros/libros-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Libros } from 'src/app/models/libros';
 import { LibroService } from 'src/app/services/libro.service';
-
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-libros-form',
   templateUrl: './libros-form.component.html',
@@ -28,7 +28,7 @@ export class LibrosFormComponent implements OnInit {
   public crear(): void {
     this.service.crearLibro(this.libro).subscribe(libro => {
       console.log(libro);
-      alert(`Libro ${libro.titulo} fue creado con éxito`);
+      Swal.fire('Nuevo Libro',`Libro ${libro.titulo} fue creado con éxito`,'success');
       this.router.navigate(['/libros']);
     }, err => {
       if (err.status === 400){
@@ -36,12 +36,14 @@ export class LibrosFormComponent implements OnInit {
         console.log(this.error);
       }
     });
+
+
   }
 
   public editar(): void {
     this.service.actualizarLibro(this.libro).subscribe(libro => {
       console.log(libro);
-      alert(`Libro ${libro.titulo} fue actualizado con éxito`);
+      Swal.fire('Libro Modificado',`Libro ${libro.titulo} fue modificado con éxito`,'success');
       this.router.navigate(['/libros']);
     }, err => {
       if (err.status === 400){
--- src/app/components/libros/libros.component.ts
+++ src/app/components/libros/libros.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Libros } from 'src/app/models/libros';
 import { LibroService } from 'src/app/services/libro.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-libros',
@@ -9,13 +10,34 @@ import { LibroService } from 'src/app/services/libro.service';
 })
 export class LibrosComponent implements OnInit {
 
-  titulo='Listado de libros';
-  libros:Libros[];
-  
-  constructor(private service:LibroService) { }
+  titulo = 'Listado de libros';
+  libros: Libros[];
+
+  constructor(private service: LibroService) { }
 
   ngOnInit(): void {
-    this.service.listarLibros().subscribe(libros =>this.libros=libros);
+    this.service.listarLibros().subscribe(libros => this.libros = libros);
+  }
+
+  public eliminar(libro: Libros): void {
+
+    Swal.fire({
+      title: 'Cuidado',
+      text: `¿Seguro que desea eliminar a ${libro.titulo} ?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, eliminar !'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.service.eliminarLibro(libro.idLibro).subscribe(() => {
+          this.libros = this.libros.filter(a => a !== libro);
+          Swal.fire('Libro eliminado', `Libro ${libro.titulo} eliminado exitosamente`, 'success');
+        });
+
+      }
+    })
   }
 
 }
